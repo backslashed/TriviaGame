fabea07b9832bc6d03b2fd9eafdc644b100f38a7
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-07-13 18:11:18 +0100

    Indentation

--- TriviaGame.rb
+++ TriviaGame.rb
@@ -4,33 +4,34 @@ class TriviaGame
   SCORE_CORRECT = 10
   SCORE_INCORRECT = -5
 
-  # Read CSV file and store tests
-  def initialize(file)
-    @tests = []
+  public
 
-    CSV.foreach(file) do |line| 
-      @tests << { question: line[0], answer: line[1].strip }
+    # Read CSV file and store tests
+    def initialize(file)
+      @tests = []
+
+      CSV.foreach(file) do |line| 
+        @tests << { question: line[0], answer: line[1].strip }
+      end
     end
-  end
 
-  # Start game with given number of levels
-  def play(number_of_games)
-    @tests.shuffle!
-    score = 0
-
-    number_of_games.times { |level| score += get_score_for @tests[level] }
-    puts "You scored #{score}/#{SCORE_CORRECT * number_of_games}"
-  end
+    # Start game with given number of levels
+    def play(number_of_games)
+      @tests.shuffle!
+      score = 0
+      number_of_games.times { |level| score += get_score_for @tests[level] }
+      puts "You scored #{score}/#{SCORE_CORRECT * number_of_games}"
+    end
 
   private
 
-  # Get score based on user input to test
-  def get_score_for(test)
-    print test[:question] + "\n> "
-    is_answer_correct?( gets, test ) ? SCORE_CORRECT : SCORE_INCORRECT
-  end
+    # Get score based on user input to test
+    def get_score_for(test)
+      print test[:question] + "\n> "
+      is_answer_correct?( gets, test ) ? SCORE_CORRECT : SCORE_INCORRECT
+    end
 
-  def is_answer_correct?(answer, test)
-    answer.chomp.downcase == test[:answer].downcase
-  end
+    def is_answer_correct?(answer, test)
+      answer.chomp.downcase == test[:answer].downcase
+    end
 end
