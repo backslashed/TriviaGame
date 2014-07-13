fe3912268438b7ec8d03c46ea99cc7a911a517d6
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-07-13 16:52:20 +0100

    Ruby-way

--- TriviaGame.rb
+++ TriviaGame.rb
@@ -4,37 +4,56 @@ class TriviaGame
   SCORE_CORRECT = 10
   SCORE_INCORRECT = -5
 
-  # Read CSV file and store tests
+  public
+
   def initialize(file)
     @tests = []
-
-    CSV.foreach(file) do |line| 
-      @tests << { question: line[0], answer: line[1].strip }
-    end
+    CSV.foreach(file) { |line| load_test(line) }
   end
 
-  # Start game with given number of levels
-  def play(number_of_games)
-    @tests.shuffle!
-    score = 0
-
-    number_of_games.times do |level|
-      score += get_score_for @tests[level]
-    end
-
-    puts "You scored #{score}/#{SCORE_CORRECT * number_of_games}"
+  # Play a round with a given number of tests
+  def play(number_of_tests)
+    return if number_of_tests > @tests.size
+    reset_game
+    run_tests(number_of_tests)
   end
 
   private
 
-  # Get score based on user input to test
-  def get_score_for(test)
-    print test[:question] + "\n> "
+  # Load a test from an array
+  def load_test(test)
+    @tests << { question: test[0], answer: test[1].strip }
+  end
 
-    if gets.chomp.downcase == test[:answer].downcase
-      SCORE_CORRECT
+  # Shuffle tests and reset score
+  def reset_game
+    @score = 0
+    @tests.shuffle!
+  end
+
+  # Run given number of tests
+  def run_tests(levels)
+    levels.times { |round| ask_question @tests[round] }
+    display_score
+  end
+
+  # Run a given test
+  def ask_question(test)
+    print test[:question] + "\n> "
+    @score += is_answer_correct?(gets, test) ? SCORE_CORRECT : SCORE_INCORRECT
+  end
+
+  # Check if the answer to a test is correct
+  def is_answer_correct?(answer, test)
+    answer.chomp.downcase == test[:answer].downcase
+  end
+
+  # Display the score to a game
+  def display_score
+    if @score >= 0
+      puts "Score: #{@score}!"
     else
-      SCORE_INCORRECT
+      puts "You've lost. Try again."
     end
   end
 end
