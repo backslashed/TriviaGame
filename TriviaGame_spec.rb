92bdd90ade70cbe3b9f40c8b159d9f375bbf32a6
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-07-13 18:52:23 +0100

    Pulled off 2 tests

--- TriviaGame_spec.rb
+++ TriviaGame_spec.rb
@@ -2,15 +2,21 @@ require "spec_helper"
 require "./TriviaGame"
 
 describe TriviaGame do
+  before :each do
+    @game = TriviaGame.new( "TriviaGame.csv" )
+  end
+
   it "should shuffle tests for new games" do
-  
+    expect_any_instance_of(Array).to receive(:shuffle!).once
+    @game.play(3)
   end
   
-  it "should put forward given number of games" do
-  
+  it "should get score for number of questions requested" do
+    expect(@game).to receive(:get_score_for).exactly(5).times.and_return(0)
+    @game.play(5)
   end
   
-  it "should output correct score when game finishes" do
+  it "should decrease score when answer is incorrect" do
 
   end
 end
